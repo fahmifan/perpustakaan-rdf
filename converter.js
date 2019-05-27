df9b01a910259ef7b7e28f954059572db76989b9
miun173 <fahmifan173@gmail.com>
2019-05-27 11:00:35 +0700

    feat: create author & pubsliher

--- converter.js
+++ converter.js
@@ -15,7 +15,7 @@ const books = [
     "Title": "advanced engineering mathematics",
     "Author": "kreyszig, erwin",
     "Library": "fakultas matematika dan ilmu pengetahuan alam",
-    "Publisher": "john wiley & sons : new york ,., 1988",
+    "Publisher": "john wiley & sons",
     "Category": "mathematic; engineering; database management"
   },
   {
@@ -23,7 +23,7 @@ const books = [
     "Title": "mathematics for the technologies with calculus",
     "Author": "clar,lawrence m",
     "Library": "fakultas matematika dan ilmu pengetahuan alam",
-    "Publisher": "prentice hall : amerika 1978",
+    "Publisher": "prentice hall",
     "Category": "calculus; mathematic"
   },
   {
@@ -31,15 +31,15 @@ const books = [
     "Title": "discrete mathematics with combinatoric",
     "Author": "anderson,james a",
     "Library": "fakultas matematika dan ilmu pengetahuan alam",
-    "Publisher": "prentice hall : new jersey., 2001",
+    "Publisher": "prentice hall",
     "Category": "discrete; mathematic"
   },
 ]
 
-function getCats(cats) {
+function getCats(books) {
   let ncats = {};
-  for (let i = 0; i < cats.length; i++) {
-    const b = cats[i];
+  for (let i = 0; i < books.length; i++) {
+    const b = books[i];
     const bcats = b.Category.split(";").map(b => b.trim())
 
     for (let j = 0; j < bcats.length; j++) {
@@ -65,7 +65,59 @@ function createCategories(books) {
   return cats;
 }
 
-console.log('categories', createCategories(books));
+function getPubs(books) {
+  let pubs = {};
+  for (let i = 0; i < books.length; i++) {
+    const b = books[i];
+    const bcats = b.Publisher.split(" ").join("_")
+    pubs[bcats] = bcats;
+  }
+
+  return pubs;
+}
+
+function createPubs(pubs) {
+  const cats = []
+  Object.keys(pubs).forEach(el => {
+    cat = `:Pub_${el}
+    a       :Publisher , owl:NamedIndividual ;
+    :name   "${el}" .
+    `
+
+    cats.push(cat);
+  });
+
+  return cats;
+}
+
+function getAuthor(auth) {
+  let auths = {};
+  for (let i = 0; i < books.length; i++) {
+    const b = books[i];
+    const bauth = b.Author.split(" ").join("_")
+    auths[bauth] = bauth;
+  }
+
+  return auths;
+}
+
+function createAuthor(auths) {
+  const nauths = []
+  Object.keys(auths).forEach(el => {
+    cat = `:Author  a               owl:Class ;
+    rdfs:subClassOf  :Person .
+    `
+
+    nauths.push(cat);
+  });
+
+  return nauths;
+}
+
+// console.log('pubs', createAuthor(getAuthor(books)))
+// createPubs(getPubs(books)).forEach(c => console.log(c))
+// console.log('pubs', getPubs(books));
+// console.log('categories', createCategories(books));
 
 return;
 
